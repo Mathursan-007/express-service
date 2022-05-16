48396d75317f7a3a867a0f6c431b93c6c8b986f2
Mathursan-007 <mathursan99@gmail.com>
2022-05-16 21:15:38 +0530

    update endpoint

--- routes/service.js
+++ routes/service.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("200 ok");
+    res.send("Payment Received");
 })
 
 export default router;
