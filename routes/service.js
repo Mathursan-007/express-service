4dbac8886e5cfa41b8273f07acf7104b3ecd8251
Mathursan-007 <mathursan99@gmail.com>
2022-05-16 23:30:00 +0530

    update k8s

--- routes/service.js
+++ routes/service.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Payment Received");
+    res.send("Payment Received via Payment-API");
 })
 
 export default router;
